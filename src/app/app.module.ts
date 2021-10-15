dec502a5015025888b8c124e6add42fc5bc4138b
Liel <lielyehosh@gmail.com>
2021-10-15 15:57:30 +0300

    Add angular app with simple component of auto complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AutoCompleteComponent} from './shared/auto-complete/auto-complete.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ClickOutsideDirective} from './shared/click-outside.directive';
+import {SearchFilterPipe} from './shared/pipes/search-filter.pipe';
+import {LetterBoldPipe} from './shared/pipes/letter-bold.pipe';
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutoCompleteComponent,
+    ClickOutsideDirective,
+    SearchFilterPipe,
+    LetterBoldPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
