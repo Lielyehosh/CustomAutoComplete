871272de3900c3dc2c4b0d0f828d1328f2fe8b6b
lielyehosh <lielyehosh@gmail.com>
2021-10-17 00:17:44 +0300

    implement a basic trie for search strings & add basic tests for it

---
+++ server/Common/Utils/AutoCompleteTrie.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+
+// This code is a C# copy of the JAVA implementation here - https://github.com/samgh/Byte-by-Byte-Solutions/blob/master/java/Autocomplete.java
+
+namespace AutoComplete.Common.Utils
+{
+    public class AutoCompleteTrie
+    {
+        // Trie node class
+        internal class Node {
+            internal string Prefix { get; set; }
+            internal Dictionary<char, Node> Children { get; set; }
+
+            internal bool IsWord { get; set; }
+
+            internal Node(string prefix) {
+                Prefix = prefix;
+                Children = new Dictionary<char, Node>();
+            }
+        }
+
+        // The trie
+        private Node Trie { get; set; }
+        
+        // Construct the trie from the dictionary
+        public AutoCompleteTrie(IEnumerable<string> dict)
+        {
+            Trie = new Node("");
+            foreach (var s in dict)
+            {
+                InsertWord(s);
+            }
+        }
+        
+        private void InsertWord(string str) {
+            var curr = Trie;
+            for (var i = 0; i < str.Length; i++) {
+                if (!curr.Children.ContainsKey(str[i])) {
+                    curr.Children[str[i]] = new Node(str[..(i+1)]);
+                }
+                curr = curr.Children[str[i]];
+                if (i == str.Length - 1) 
+                    curr.IsWord = true;
+            }
+        }
+        
+        
+        // Find all words in trie that start with prefix
+        public IEnumerable<string> GetWordsForPrefix(string pre) {
+            var results = new LinkedList<string>();
+        
+            // Iterate to the end of the prefix
+            var curr = Trie;
+            foreach (var ch in pre.ToCharArray())
+            {
+                if (curr.Children.ContainsKey(ch)) {
+                    curr = curr.Children[ch];
+                } else {
+                    return results;
+                }
+            }
+        
+            // At the end of the prefix, find all child words
+            FindAllChildWords(curr, results);
+            return results;
+        }
+
+        private void FindAllChildWords(Node node, LinkedList<string> results)
+        {
+            if (node.IsWord) 
+                results.AddLast(node.Prefix);
+            foreach (var key in node.Children.Keys)
+            {
+                FindAllChildWords(node.Children[key], results);
+            }
+        }
+    }
+}
---
+++ server/Tests/AutoCompleteTrieTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoComplete.Common.Utils;
+using NUnit.Framework;
+
+namespace AutoComplete.Tests
+{
+    public class AutoCompleteTrieTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            NormalList = new string[] {"abc", "acd", "bcd", "def", "a", "aba"};
+            EmptyList = new string[] {};
+        }
+
+        private string[] EmptyList { get; set; }
+        private string[] NormalList { get; set; }
+
+        [Test]
+        [TestCase(new[] {"abc"}, "abc")]
+        [TestCase(new[] {"abc", "acd", "bcd", "def", "a", "aba"}, "")]
+        [TestCase(new[] {"def"}, "def")]
+        [TestCase(new string[] {}, "defa")]
+        public void Tests(string[] expectedResults, string prefix) {
+            var autoCompleteTrie = new AutoCompleteTrie(NormalList);
+            var results = autoCompleteTrie.GetWordsForPrefix(prefix);
+            Assert.IsTrue(CompareArrays(results,expectedResults));
+        }
+
+        public bool CompareArrays(IEnumerable<string> A, IEnumerable<string> B)
+        {
+            if (A.Count() != B.Count()) 
+                return false;
+
+            var aOrdered = A.OrderBy(s => s).ToList();
+            var bOrdered = B.OrderBy(s => s).ToList();
+        
+        
+            for (int i = 0; i < aOrdered.Count(); i++) {
+                if (aOrdered[i] != bOrdered[i]) 
+                    return false;
+            }
+            return true;
+        }
+    }
+}
