e2120b513aab292ddec4eee7b7a37114d6db67b9
lielyehosh <lielyehosh@gmail.com>
2021-10-16 20:56:48 +0300

    make the 'geonameid' search field

--- server/Models/City.cs
+++ server/Models/City.cs
@@ -11,6 +11,7 @@ namespace AutoComplete.Models
         public string Name { get; set; }
         public string Country { get; set; }
         public string SubCountry { get; set; }
+        [SearchField]
         public string GeoNameId { get; set; }
 
         public override string ToLabel()
